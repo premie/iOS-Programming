72fac79c5ab67ca8373855ed58b0dfe4f70a1f25
Andrew Long <along27@uic.edu>
2012-12-27 11:51:17 -0600

    Initial Chapter 2 project.

---
+++ Chapter 2/RandomPossessions/RandomPossessions/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RandomPossessions
+//
+//  Created by Andrew Long on 12/27/12.
+//  Copyright (c) 2012 Andrew Long. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
