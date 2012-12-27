8aa12437c13038b3da4eab43a4f8c6dcdd58cf07
Andrew Long <along27@uic.edu>
2012-12-27 12:54:48 -0600

    Initial Chapter 2 - Silver Challenge project added.

---
+++ Chapter 2/RandomPossessions - Silver Challenge/BNRItem.m
@@ -0,0 +1,95 @@
+//
+//  BNRItem.m
+//  RandomPossessions
+//
+//  Created by Andrew Long on 12/27/12.
+//  Copyright (c) 2012 Andrew Long. All rights reserved.
+//
+
+#import "BNRItem.h"
+
+@implementation BNRItem
+
++(id)randomItem{
+    // Create an array of three adjectives
+    NSArray* randomAdjectiveList = [NSArray arrayWithObjects:@"Fluffly",@"Rusty",@"Shiny",nil];
+    
+    // Create an array of three nouns
+    NSArray* randomNounList = [NSArray arrayWithObjects:@"Bear",@"Spork",@"Mac",nil];
+    
+    // Get the index of a random adjective/noun from the lists
+    NSInteger adjectiveIndex = rand() % [randomAdjectiveList count];
+    NSInteger nounIndex = rand() % [randomNounList count];
+    
+    NSString* randomName = [NSString stringWithFormat:@"%@ %@",[randomAdjectiveList objectAtIndex:adjectiveIndex],[randomNounList objectAtIndex:nounIndex]];
+    
+    int randomValue = rand() % 100;
+    
+    NSString* randomSerialNumber = [NSString stringWithFormat:@"%c%c%c%c%c",
+                                    '0' + rand() % 10,
+                                    'A' + rand() % 26,
+                                    '0' + rand() % 10,
+                                    'A' + rand() % 26,
+                                    '0' + rand() % 10];
+    
+    BNRItem* newItem = [[self alloc] initWithItemName:randomName valueInDollars:randomValue serialNumber:randomSerialNumber];
+    
+    return newItem;
+}
+
+-(id)init{
+    return [self initWithItemName:@"Item"
+                   valueInDollars:0
+                     serialNumber:@""];
+}
+
+-(id)initWithItemName:(NSString *)name
+       valueInDollars:(int)value
+         serialNumber:(NSString *)sNumber{
+    // Call the superclass's designated initializer
+    self = [super init];
+    if(self){
+        // Give the instance variables initial values
+        [self setItemName:name];
+        [self setSerialNumber:sNumber];
+        [self setValueInDollars:value];
+        dateCreated = [[NSDate alloc] init];
+    }
+    
+    return self;
+}
+
+-(void)setItemName:(NSString *)str{
+    itemName = str;
+}
+
+-(NSString*)itemName{
+    return itemName;
+}
+
+-(void)setSerialNumber:(NSString *)str{
+    serialNumber = str;
+}
+
+-(NSString*)serialNumber{
+    return serialNumber;
+}
+
+-(void)setValueInDollars:(int)i{
+    valueInDollars = i;
+}
+
+-(int)valueInDollars{
+    return valueInDollars;
+}
+
+-(NSDate*)dateCreated{
+    return dateCreated;
+}
+
+-(NSString*)description{
+    NSString* descriptionString = [[NSString alloc] initWithFormat:@"%@ (%@): Worth $%d, recorded on %@",itemName,serialNumber,valueInDollars,dateCreated];
+    return descriptionString;
+}
+
+@end
