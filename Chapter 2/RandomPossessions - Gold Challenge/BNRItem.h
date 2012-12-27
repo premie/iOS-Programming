c9ed072b571b0813d3d00dd6c95b9d197fc19d74
Andrew Long <along27@uic.edu>
2012-12-27 13:03:32 -0600

    Initial Chapter 2 Gold Challenge project added.

---
+++ Chapter 2/RandomPossessions - Gold Challenge/BNRItem.h
@@ -0,0 +1,35 @@
+//
+//  BNRItem.h
+//  RandomPossessions
+//
+//  Created by Andrew Long on 12/27/12.
+//  Copyright (c) 2012 Andrew Long. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRItem : NSObject{
+    NSString* itemName;
+    NSString* serialNumber;
+    int valueInDollars;
+    NSDate* dateCreated;
+}
+
++(id)randomItem;
+
+-(id)initWithItemName:(NSString*)name
+       valueInDollars:(int)value
+         serialNumber:(NSString*)sNumber;
+
+-(void)setItemName:(NSString*)str;
+-(NSString*)itemName;
+
+-(void)setSerialNumber:(NSString*)str;
+-(NSString*)serialNumber;
+
+-(void)setValueInDollars:(int)i;
+-(int)valueInDollars;
+
+-(NSDate*)dateCreated;
+
+@end
