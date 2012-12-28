96fc319c4e84f5931dd6b112bc9fcc7154a7a568
Andrew Long <along27@uic.edu>
2012-12-28 15:12:56 -0600

    Initial Chapter 4 project.

---
+++ Chapter 4/Whereami/Whereami/WhereamiViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhereamiViewController.h
+//  Whereami
+//
+//  Created by Andrew Long on 12/28/12.
+//  Copyright (c) 2012 Andrew Long. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WhereamiViewController : UIViewController
+
+@end
