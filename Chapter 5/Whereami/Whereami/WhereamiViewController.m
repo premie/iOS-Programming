1af4aca284fe6b2cfc4a7439a9a7c526b92fbda9
Andrew Long <along27@uic.edu>
2012-12-29 20:56:16 -0600

    MKMapView and animated region zoom implemented.

--- Chapter 5/Whereami/Whereami/WhereamiViewController.m
+++ Chapter 5/Whereami/Whereami/WhereamiViewController.m
@@ -14,20 +14,16 @@
 
 @implementation WhereamiViewController
 
+-(void)viewDidLoad{
+    [worldView setShowsUserLocation:YES];
+}
+
 -(id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil{
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if(self){
-        // Create a location manager object
         locationManager = [[CLLocationManager alloc] init];
-        
         [locationManager setDelegate:self];
-        
-        // And we want it to be as accurate as possible
-        // regardless of how much time/power it takes
         [locationManager setDesiredAccuracy:kCLLocationAccuracyBest];
-        
-        // Tell our manager to start looking for its location imediately
-        [locationManager startUpdatingLocation];
     }
     
     return self;
@@ -47,6 +43,13 @@
 -(void)locationManager:(CLLocationManager*)manager
       didFailWithError:(NSError *)error{
     NSLog(@"Could not find location: %@", error);
+}
+
+-(void)mapView:(MKMapView *)mapView
+didUpdateUserLocation:(MKUserLocation *)userLocation{
+    CLLocationCoordinate2D loc = [userLocation coordinate];
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(loc, 250, 250);
+    [worldView setRegion:region animated:YES];
 }
 
 @end
