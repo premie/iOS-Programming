ba905b22a1e52ca80c029c148e742680fb87ed1a
Andrew Long <along27@uic.edu>
2012-12-31 01:38:31 -0600

    Initial Chapter 6 project.

---
+++ Chapter 6/Hypnosister/Hypnosister/HypnosisterAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HypnosisterAppDelegate.h
+//  Hypnosister
+//
+//  Created by Andrew Long on 12/31/12.
+//  Copyright (c) 2012 Andrew Long. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnosisterAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
