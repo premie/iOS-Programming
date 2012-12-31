d4a3e15cecdd4e489bc484ce4f9333bafb56e751
Andrew Long <along27@uic.edu>
2012-12-31 03:57:51 -0600

    Initial Chapter 6 Bronze Challenge project.

---
+++ Chapter 6/Hypnosister - Bronze Challenge/Hypnosister/HypnosisView.h
@@ -0,0 +1,16 @@
+//
+//  HypnosisView.h
+//  Hypnosister
+//
+//  Created by Andrew Long on 12/31/12.
+//  Copyright (c) 2012 Andrew Long. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HypnosisView : UIView{
+    
+}
+@property (nonatomic, strong) UIColor* circleColor;
+
+@end
