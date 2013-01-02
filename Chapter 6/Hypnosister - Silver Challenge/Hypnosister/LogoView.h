df35dda2bd1c873bb11b4c00d28856a4267be267
Andrew Long <along27@uic.edu>
2013-01-02 01:55:53 -0600

    Initial (almost) Chapter 6 Gold Challenge project.

---
+++ Chapter 6/Hypnosister - Silver Challenge/Hypnosister/LogoView.h
@@ -0,0 +1,13 @@
+//
+//  LogoView.h
+//  Hypnosister
+//
+//  Created by Andrew Long on 1/2/13.
+//  Copyright (c) 2013 Andrew Long. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LogoView : UIView
+
+@end
---
+++ Chapter 6/Hypnosister - Silver Challenge/Hypnosister/LogoView.m
@@ -0,0 +1,26 @@
+//
+//  LogoView.m
+//  Hypnosister
+//
+//  Created by Andrew Long on 1/2/13.
+//  Copyright (c) 2013 Andrew Long. All rights reserved.
+//
+
+#import "LogoView.h"
+
+@implementation LogoView
+
+-(id)initWithFrame:(CGRect)frame{
+    self = [super initWithFrame:frame];
+    if(self){
+        // Set the background to clear
+        [self setBackgroundColor:[UIColor clearColor]];
+    }
+    return self;
+}
+
+-(void)drawRect:(CGRect)rect{
+        
+}
+
+@end
