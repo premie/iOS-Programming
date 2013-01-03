76358e33c8b4295f236422aee9c181a0fc5af808
Andrew Long <along27@uic.edu>
2013-01-03 00:29:26 -0600

    Initial Chapter 7 Bronze Challenge project.

---
+++ Chapter 7/HypnoTime - Bronze Challenge/HypnoTime/HypnoAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HypnoAppDelegate.h
+//  HypnoTime
+//
+//  Created by Andrew Long on 1/2/13.
+//  Copyright (c) 2013 Andrew Long. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnoAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Chapter 7/HypnoTime - Bronze Challenge/HypnoTime/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HypnoTime
+//
+//  Created by Andrew Long on 1/2/13.
+//  Copyright (c) 2013 Andrew Long. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HypnoAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HypnoAppDelegate class]));
+    }
+}
