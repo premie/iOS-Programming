1e456a7982fdd4765589345d16f1c1755906b2de
Andrew Long <along27@uic.edu>
2013-01-03 00:28:13 -0600

    Finished Chapter 7 project.

---
+++ Chapter 7/HypnoTime/HypnoTime/TimeViewController.m
@@ -0,0 +1,13 @@
+//
+//  TimeViewController.m
+//  HypnoTime
+//
+//  Created by Andrew Long on 1/2/13.
+//  Copyright (c) 2013 Andrew Long. All rights reserved.
+//
+
+#import "TimeViewController.h"
+
+@implementation TimeViewController
+
+@end
---
+++ Chapter 7/HypnoTime/HypnosisViewController.m
@@ -0,0 +1,13 @@
+//
+//  HypnosisViewController.m
+//  HypnoTime
+//
+//  Created by Andrew Long on 1/2/13.
+//  Copyright (c) 2013 Andrew Long. All rights reserved.
+//
+
+#import "HypnosisViewController.h"
+
+@implementation HypnosisViewController
+
+@end
