5ca93751837232f2c66d056c25930814dd989194
Andrew Long <along27@uic.edu>
2013-01-04 01:44:42 -0600

    Initial Chapter 8 Bronze Challenge project.

---
+++ Chapter 8/HeavyRotation - Bronze Challenge/HeavyRotation/RotationAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RotationAppDelegate.h
+//  HeavyRotation
+//
+//  Created by Andrew Long on 1/4/13.
+//  Copyright (c) 2013 Andrew Long. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RotationAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
