4244bb58bf191c046f9bb7eb39e89e1114f0e4d1
Andrew Long <along27@uic.edu>
2013-01-04 01:02:41 -0600

    Initial Chapter 8 project.

---
+++ Chapter 8/HeavyRotation/HeavyRotation/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HeavyRotation
+//
+//  Created by Andrew Long on 1/4/13.
+//  Copyright (c) 2013 Andrew Long. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RotationAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RotationAppDelegate class]));
+    }
+}
